cd73faa8c73b3ef067b0b15feb5617a19c05e1db
Squidboss1 <xxx147ac@gmail.com>
2023-12-09 21:34:38 +0100

    Added listsort1

---
+++ listsort1.py
@@ -0,0 +1,12 @@
+#list to sort
+my_list = [8, 10, 6, 2, 4]
+swapped = True
+
+while swapped:
+    swapped = False  # no swaps so far
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True  # a swap occurred!
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
