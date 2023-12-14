d9f6aaa6e92a805c9ee3dd49abb8b72c0025ec89
Squidboss1 <xxx147ac@gmail.com>
2023-12-14 15:41:19 +0100

    Added sol 1.1

---
+++ 1.1.py
@@ -0,0 +1,7 @@
+
+def rec_area():
+    rec_height = float(input("Podaj wysokość prostokątu: "))
+    rec_width = float(input("Podaj szerokość prostokątu: "))
+    return print(rec_width * rec_height)
+
+rec_area()
