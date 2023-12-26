c78d7fe887d3c1c78d5f55f4b8520f118407a7c1
Squidboss1 <xxx147ac@gmail.com>
2023-12-26 14:38:16 +0100

    added sol 1.2.

---
+++ 1.2.py
@@ -0,0 +1,10 @@
+import math
+
+print("Program wyświetla stałą pi z dokładnością do 3 miejsc po przecinku.")
+
+# print("pi = %5.3f"% math.pi, ".", sep="")
+
+pi_with_three_decimals = round(math.pi, 3)
+
+print(pi_with_three_decimals)
+
